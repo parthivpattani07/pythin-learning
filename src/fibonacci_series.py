2f8e19e29e9ba259ec6c60dc36daa260a8a4d7ac
parthivpattani07 <parthivpattani07@gmail.com>
2026-07-31 00:09:40 +0530

    fix: code output

--- src/fibonacci_series.py
+++ src/fibonacci_series.py
@@ -4,14 +4,22 @@ def fibonacci(x):
     for _ in range(2,x):
         sequence.append(sequence[-1]+sequence[-2])
     
+    if x==0 :
+        return []
+    if x==1:
+        return [0]
+
     return sequence
         
         
+"""
+input:
+print(fibonacci(7))
+print(fibonacci(1))
+print(fibonacci(0))
+output: 
+[ 0 1 1 2 3 5 8 ]
+[0]
+[]
 
-
-
-        
-
-
-
-print(fibonacci(5))
+"""
