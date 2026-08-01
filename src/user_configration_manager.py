16994b3605fee4a9bc65b310877753ca05f44028
parthivpattani07 <parthivpattani07@gmail.com>
2026-08-01 22:41:18 +0530

    new: added to config. manager
    Refactor add_setting function to accept a tuple and check for existing keys.

--- src/user_configration_manager.py
+++ src/user_configration_manager.py
@@ -1,7 +1,21 @@
-test_settings={('theme':'light'),('THEME', 'dark'),('volume', 'high')}
+test_settings={
+    'theme':'light'
+}
+test_tuple=('THEME','dark')
     
     
     
-    def add_setting(key,value):
-    key=key.lower()
-    value=value.lower()
+def add_setting(settings_dict,kv_tuple):
+    
+    raw_key, value = kv_tuple
+    key = raw_key.lower()
+    if key in settings_dict:
+        return ( f 'Setting {raw_key} already exists! Cannot add a new setting with this name.')
+    settings_dict[key] = value
+    return (f"Setting {key} successfully added!")
+
+    
+
+
+print(add_setting(test_settings,test_tuple))
+
