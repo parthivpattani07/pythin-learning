87f7967114bae19be0a9e36de9d9c3fab6aa1ce4
parthivpattani07 <parthivpattani07@gmail.com>
2026-08-01 22:25:13 +0530

    fix : comments
    Updated output comments to use correct syntax.

--- src/number_pattern.py
+++ src/number_pattern.py
@@ -11,9 +11,12 @@ def number_pattern(n):
 
     return result.strip()   
 
+"""
+input:
 print(number_pattern(4))
 print(number_pattern(12))
 
-#output:  
-#1 2 3 4
-#1 2 3 4 5 6 7 8 9 10 11 12
+output:  
+1 2 3 4
+1 2 3 4 5 6 7 8 9 10 11 12
+"""
