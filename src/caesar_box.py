572b49b08a56c5bc06b4c837707643f72243d488
parthivpattani07 <parthivpattani07@gmail.com>
2026-08-04 23:46:37 +0530

    modify: caesar box code

--- src/caesar_box.py
+++ src/caesar_box.py
@@ -1,23 +1,21 @@
 code1='tuud rrra eeit sbex'
 code2='TETOIUIN HBOBNTRO EETETYDW FLHLHORA UOOIEFEN TNSEBTAD UGWVEHMF RSHEAESX'
 
-def caesar_box(code,size):       # size is always squre root of total letters.
-
+def caesar_box(code):  
     code=code.replace(" ","")
-    code_0=code[0::size]+ code[1::size]+ code[2::size]+code[3::size]
+    size=int(len(code)**0.5)
     if not isinstance(size,int):
-        return 'size should be an integer '
+        return 'Size should be an integer. '
+    code_0=code[0::size]+ code[1::size]+ code[2::size]+code[3::size]
+    
     
     return code_0 +" "+ "The code is decihpherd and now breaking it into words will give us the hidden message." 
-
 """
 input:
-print(caesar_box(code1,4))
-print(caesar_box(code2,8))
-
-#output:  
-tresureburiedatx 
-The code is decihpherd and now breaking it into words will give us the hidden message. 
-THEFUTUREBELONGSTOTHOSEWHOBELIEVE
-The code is decihpherd and now breaking it into words will give us the hidden message.
+print(caesar_box(code1))
+print(caesar_box(code2))
+output:
+trsureburiedatx The code is decihpherd and now breaking it into words will give us the hidden message.
+FUTUREBELONGSTOTHOSEWHOBELIEVE The code is decihpherd and now breaking it into words will give us the hidden message.
 """
+
