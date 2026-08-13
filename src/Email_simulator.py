b3beaa7a09d6662fd95d4ac59a3bbd90a55b54bb
parthivpattani07 <parthivpattani07@gmail.com>
2026-08-13 22:53:21 +0530

    add: code to program

--- src/Email_simulator.py
+++ src/Email_simulator.py
@@ -8,8 +8,21 @@ class Email:
 
     def mark_as_read(self):
         self.read = True
+    
 
 class User:
     def __init__(self, name):
         self.name = name
-        
+        self.inbox = Inbox()
+
+    def send_email(self, receiver, subject, body):
+        email = Email(sender=self, receiver=receiver, subject=subject, body=body)
+        receiver.inbox.receive_email(email)
+
+class Inbox:
+    def __init__(self):
+        self.emails = []
+    
+    def receive_email(self, email):
+        self.emails.append(email)
+
