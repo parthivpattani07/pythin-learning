d2f0f2b19bb32acab88ef0e9d03cd0cb6ad5a47e
parthivpattani07 <parthivpattani07@gmail.com>
2026-08-18 18:44:26 +0530

    modify: base change algorithm
    Refactor change_base function to simplify base conversion logic.

--- src/base_change_algorithm.py
+++ src/base_change_algorithm.py
@@ -1,33 +1,22 @@
 import math
 
 def change_base(value, initial_base, final_base):
-    if initial_base == 10:
-        y = ""
 
-        while value > 0:
-            x = math.floor(value / final_base)
-            r = value - final_base * x
-            y = str(r) + y
-            value = x
+    new_value = 0
 
-        return y if y else "0"
+        
+    for digit in str(value):
+         new_value = new_value * initial_base + int(digit)
 
-    else:
-        new_value = 0
+        
+    y = ""
+    while new_value > 0:
+         x = math.floor(new_value / final_base)
+         r = new_value - final_base * x
+         y = str(r) + y
+         new_value = x
 
-        # Convert initial base → base 10
-        for digit in str(value):
-            new_value = new_value * initial_base + int(digit)
-
-        # Convert base 10 → final base
-        y = ""
-        while new_value > 0:
-            x = math.floor(new_value / final_base)
-            r = new_value - final_base * x
-            y = str(r) + y
-            new_value = x
-
-        return y if y else "0"
+    return y if y else "0"
 
 """
 input:
