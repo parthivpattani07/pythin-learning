41b94584876ba74ad1223c2be9822e1b98b66bf9
parthivpattani07 <parthivpattani07@gmail.com>
2026-08-28 23:08:22 +0530

    new:media catalouge

---
+++ src/media_catalouge.py
@@ -0,0 +1,9 @@
+class Movie:
+    def __init__(self, title, year, director, duration):
+        self.title = title
+        self.year = year
+        self.director = director
+        self.duration = duration
+    
+    def __str__(self):
+        return f"{self.title} {(self.year)} - {self.duration} min, {self.director}"
